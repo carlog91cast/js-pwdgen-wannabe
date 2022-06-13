0db98e4e8b699dd3c782a0341e83025f1a4db53e
bigeppo <carlog91cast@gmail.com>
2022-06-13 17:26:55 +0200

    add suggested psw

--- js/main.js
+++ js/main.js
@@ -1,1 +1,22 @@
-const userName = prompt('Inserisci il tuo nome');
+
+// username
+
+const userName = prompt('Add Name');
+
+document.getElementById('user-name').innerHTML = userName;
+
+// usersurname
+
+const userSurname = prompt('Add Surname');
+
+document.getElementById('user-surname').innerHTML = userSurname;
+
+// preferred-color
+
+const userColor = prompt('Add preferite color');
+
+document.getElementById('user-color').innerHTML = userColor;
+
+// suggested-psw
+
+const userPsw = "Add Name" + "Add Surname" + "Add preferite color"
